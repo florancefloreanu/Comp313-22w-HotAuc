74fd2fc15dfc654bc362a7b1e21903f06c58fd7e
Kenneth <kennethlorenzg@gmail.com>
2022-02-12 12:18:07 -0500

    added brand field
    Added brand field

--- client/src/components/dashboard-component/layouts/Dashboard-bid.js
+++ client/src/components/dashboard-component/layouts/Dashboard-bid.js
@@ -1,80 +1,81 @@
-import { Link } from "react-router-dom"
-import React, { useEffect, useState } from "react"
-import { useSelector, useDispatch } from "react-redux"
-import Moment from "react-moment"
-import axios from "axios"
-import { SERVER_URL } from "../../../ConstantValue"
+import { Link } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import Moment from "react-moment";
+import axios from "axios";
+import { SERVER_URL } from "../../../ConstantValue";
 
 function DashboardBid() {
-	const userId = useSelector((state) => state.userInfor.user._id)
-	//Set dispatch for Redux
-	const [items, setItems] = useState([])
+  const userId = useSelector((state) => state.userInfor.user._id);
+  //Set dispatch for Redux
+  const [items, setItems] = useState([]);
 
-	const [loading, setLoading] = useState(true)
+  const [loading, setLoading] = useState(true);
 
-	useEffect(() => {
-		const fetchItems = async () => {
-			console.log("called")
-			//Set request header
-			const config = {
-				headers: {
-					"Content-Type": "Application/json"
-				}
-			}
-			console.log(userId)
-			const res = await axios.get(`${SERVER_URL}item/bidder/${userId}`, config)
-			setItems(res.data)
+  useEffect(() => {
+    const fetchItems = async () => {
+      console.log("called");
+      //Set request header
+      const config = {
+        headers: {
+          "Content-Type": "Application/json",
+        },
+      };
+      console.log(userId);
+      const res = await axios.get(`${SERVER_URL}item/bidder/${userId}`, config);
+      setItems(res.data);
 
-			console.log(res.data)
-			setLoading(false)
-		}
+      console.log(res.data);
+      setLoading(false);
+    };
 
-		fetchItems()
-	}, [])
+    fetchItems();
+  }, []);
 
-	const page = (
-		<div className="dashboard-bid">
-			{items?.map((item) => {
-				console.log(item)
-				console.log(item.bids)
-				let maxPrice = Math.max.apply(
-					Math,
-					item.bids.map(function (o) {
-						return o.price
-					})
-				)
-				let myPrice = item.bids.map(function (o) {
-					if (o.bidder == userId) {
-						return o.price
-					}
-				})
-				console.log(maxPrice)
-				console.log(myPrice[0])
+  const page = (
+    <div className="dashboard-bid">
+      {items?.map((item) => {
+        console.log(item);
+        console.log(item.bids);
+        let maxPrice = Math.max.apply(
+          Math,
+          item.bids.map(function (o) {
+            return o.price;
+          })
+        );
+        let myPrice = item.bids.map(function (o) {
+          if (o.bidder == userId) {
+            return o.price;
+          }
+        });
+        console.log(maxPrice);
+        console.log(myPrice[0]);
 
-				return (
-					<div className="card">
-						<div className="card-body">
-							{item.images.map((prop) => (
-								<img src={item.images[0].uri} alt="hotwheels image" />
-							))}
-							<p> </p>
-							<h2>{item.title}</h2>
-							<p>Color: {item.color}</p>
-							<p>Description: {item.description}</p>
-							<p>Year: {item.year}</p>
-							<br />
-							<p>Current Price: {item.currentPrice}</p>
-							<p>
-								<Moment parse="YYYY-MM-DD">{item.endTime}</Moment>
-							</p>
-						</div>
-					</div>
-				)
-			})}
-		</div>
-	)
+        return (
+          <div className="card">
+            <div className="card-body">
+              {item.images.map((prop) => (
+                <img src={item.images[0].uri} alt="hotwheels image" />
+              ))}
+              <p> </p>
+              <h2>{item.title}</h2>
+              <p>Brand: {item.brand}</p>
+              <p>Color: {item.color}</p>
+              <p>Description: {item.description}</p>
+              <p>Year: {item.year}</p>
+              <br />
+              <p>Current Price: {item.currentPrice}</p>
+              <p>
+                <Moment parse="YYYY-MM-DD">{item.endTime}</Moment>
+              </p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
 
-	return <>{!loading && page}</>
+  return <>{!loading && page}</>;
 }
 
-export default DashboardBid
+export default DashboardBid;
--- client/src/components/dashboard-component/layouts/Dashboard-sell.js
+++ client/src/components/dashboard-component/layouts/Dashboard-sell.js
@@ -1,60 +1,61 @@
-import React, { useEffect, useState } from "react"
-import { useSelector, useDispatch } from "react-redux"
+import React, { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import {
-	setSearchResult,
-	setResultError
-} from "../../../redux/features/dashboardItemsSlice"
-import { Link } from "react-router-dom"
-import { SERVER_URL } from "../../../ConstantValue"
-import axios from "axios"
+  setSearchResult,
+  setResultError,
+} from "../../../redux/features/dashboardItemsSlice";
+import { Link } from "react-router-dom";
+import { SERVER_URL } from "../../../ConstantValue";
+import axios from "axios";
 
 function DashboardSell() {
-	const userId = useSelector((state) => state.userInfor.user._id)
-	//Set dispatch for Redux
-	const [items, setItems] = useState([])
+  const userId = useSelector((state) => state.userInfor.user._id);
+  //Set dispatch for Redux
+  const [items, setItems] = useState([]);
 
-	useEffect(() => {
-		const fetchItems = async () => {
-			console.log("called get sells")
-			//Set request header
-			const config = {
-				headers: {
-					"Content-Type": "Application/json"
-				}
-			}
+  useEffect(() => {
+    const fetchItems = async () => {
+      console.log("called get sells");
+      //Set request header
+      const config = {
+        headers: {
+          "Content-Type": "Application/json",
+        },
+      };
 
-			const res = await axios.get(`${SERVER_URL}item/userid/${userId}`, config)
-			console.log(res.data)
-			setItems(res.data)
-		}
+      const res = await axios.get(`${SERVER_URL}item/userid/${userId}`, config);
+      console.log(res.data);
+      setItems(res.data);
+    };
 
-		fetchItems()
-	}, [])
+    fetchItems();
+  }, []);
 
-	return (
-		<div className="dashboard-sell">
-			{items?.map((item) => {
-				return (
-					<div className="card">
-						<div className="card-body">
-							{item.images.map((prop) => (
-								<img src={item.images[0].uri} alt="hotwheels image" />
-							))}
-							<p> </p>
-							<h2>{item.title}</h2>
-							<p>Color: {item.color}</p>
-							<p>Description: {item.description}</p>
-							<p>Year: {item.year}</p>
-							<p>Current Price: {item.currentPrice}</p>
-							<p>End Time: {item.endTime}</p>
-						</div>
-					</div>
-				)
-			})}
+  return (
+    <div className="dashboard-sell">
+      {items?.map((item) => {
+        return (
+          <div className="card">
+            <div className="card-body">
+              {item.images.map((prop) => (
+                <img src={item.images[0].uri} alt="hotwheels image" />
+              ))}
+              <p> </p>
+              <h2>{item.title}</h2>
+              <p>Brand: {item.brand}</p>
+              <p>Color: {item.color}</p>
+              <p>Description: {item.description}</p>
+              <p>Year: {item.year}</p>
+              <p>Current Price: {item.currentPrice}</p>
+              <p>End Time: {item.endTime}</p>
+            </div>
+          </div>
+        );
+      })}
 
-			<Link to="/dashboard/bid/edit"> Go to post a new sell</Link>
-		</div>
-	)
+      <Link to="/dashboard/bid/edit"> Go to post a new sell</Link>
+    </div>
+  );
 }
 
-export default DashboardSell
+export default DashboardSell;
