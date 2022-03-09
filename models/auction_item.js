ed52a0adc9ff7ae23b0c4bf7b288ef18a9264574
hanhan9334 <lhtony9334@126.com>
2022-03-09 18:07:45 -0500

    enable timestamp

--- models/auction_item.js
+++ models/auction_item.js
@@ -1,79 +1,82 @@
-const mongoose = require("mongoose");
+const mongoose = require("mongoose")
 
-const AuctionItemSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-  },
-  brand: {
-    type: String,
-    required: true,
-  },
-  seller: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "user",
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  color: {
-    type: String,
-    required: true,
-  },
-  year: {
-    type: String,
-    required: true,
-    default:"2000"
-  },
+const AuctionItemSchema = new mongoose.Schema(
+	{
+		title: {
+			type: String,
+			required: true
+		},
+		brand: {
+			type: String,
+			required: true
+		},
+		seller: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "user"
+		},
+		description: {
+			type: String,
+			required: true
+		},
+		color: {
+			type: String,
+			required: true
+		},
+		year: {
+			type: String,
+			required: true,
+			default: "2000"
+		},
 
-  endTime: {
-    type: Date,
-    required: true,
-  },
-  startingPrice: {
-    type: Number,
-    required: true,
-  },
-  currentPrice: {
-    type: Number,
-    required: true,
-  },
-  images: [
-    {
-      uri: {
-        type: String,
-      },
-    },
-  ],
-  bids: [
-    {
-      price: {
-        type: Number,
-      },
-      bidder: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "user",
-      },
-      bidTime: {
-        type: Date,
-      },
-    },
-  ],
-  winner: {
-    price: {
-      type: Number,
-    },
-    bidder: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "user",
-    },
-    bidTime: {
-      type: Date,
-    },
-  },
-});
+		endTime: {
+			type: Date,
+			required: true
+		},
+		startingPrice: {
+			type: Number,
+			required: true
+		},
+		currentPrice: {
+			type: Number,
+			required: true
+		},
+		images: [
+			{
+				uri: {
+					type: String
+				}
+			}
+		],
+		bids: [
+			{
+				price: {
+					type: Number
+				},
+				bidder: {
+					type: mongoose.Schema.Types.ObjectId,
+					ref: "user"
+				},
+				bidTime: {
+					type: Date
+				}
+			}
+		],
+		winner: {
+			price: {
+				type: Number
+			},
+			bidder: {
+				type: mongoose.Schema.Types.ObjectId,
+				ref: "user"
+			},
+			bidTime: {
+				type: Date
+			}
+		}
+	},
+	{ timestamps: true }
+)
 
-const AuctionItem = mongoose.model("auction-item", AuctionItemSchema);
+const AuctionItem = mongoose.model("auction-item", AuctionItemSchema)
 
-module.exports = AuctionItem;
+module.exports = AuctionItem
