f659513b179bb86cb934ccfed431940d3bbf48a9
SeyeongPark <lynn.yeong.102@gmail.com>
2022-03-17 01:22:54 -0400

    winning frontend

--- client/src/components/dashboard-component/layouts/Dashboard-win.js
+++ client/src/components/dashboard-component/layouts/Dashboard-win.js
@@ -22,7 +22,7 @@ function DashboardWin() {
 				}
 			}
 			console.log(userId)
-			const res = await axios.get(`${SERVER_URL}item/bidder/${userId}`, config)
+			const res = await axios.get(`${SERVER_URL}item/${userId}/winning`, config)
 			setItems(res.data)
 
 			console.log(res.data)
@@ -68,21 +68,6 @@ function DashboardWin() {
 
 
                             {/* Here Paypal API */}
-							 <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-								<input type="hidden" name="cmd" value="_xclick"/>
-								<input type="hidden" name="business" value="lynn.yeong.102@gmail.com"/>
-								<input type="hidden" name="lc" value="US"/>
-								<input type="hidden" name="item_name" value={item.title}/>
-								<input type="hidden" name="amount" value="1"/>
-								<input type="hidden" name="currency_code" value="CAD"/>
-								<input type="hidden" name="button_subtype" value="services"/>
-								<input type="hidden" name="no_note" value="0"/>
-								<input type="hidden" name="tax_rate" value="0.000"/>
-								<input type="hidden" name="shipping" value="0.00"/>
-								<input type="hidden" name="bn" value="PP-BuyNowBF:btn_buynowCC_LG.gif:NonHostedGuest"/>
-								<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!"/>
-								<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1"/>
-							</form> 
 						</div>
 					</div>
 				)
